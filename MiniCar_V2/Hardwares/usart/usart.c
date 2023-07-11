a7139a4fa64294d8a6e4a14328b97718bb394d09
l18005973661 <3051512493@qq.com>
2023-07-11 17:14:38 +0800

    Submit the tested uart0 and uart1 initialize

--- MiniCar_V2/Hardwares/usart/usart.c
+++ MiniCar_V2/Hardwares/usart/usart.c
@@ -1,6 +1,6 @@
 #include "usart.h"
 
-//��ӳ��printf����
+//重定向printf函数
 int fputc(int ch, FILE *f)
 {
     UARTCharPut(UART0_BASE, ch);
@@ -13,33 +13,68 @@ int fgetc(FILE *f)
 }
 
 
+u8 UART0_RX_BUF[UART_REC_LEN] = {0};  //串口接收缓冲区,最大UART_REC_LEN个字节.
+u16 UART0_RX_STA = 0;//接收到的有效字节数目
+u8 uart0RXTime = 0xFF;//串口消息间隔计时，初始化把时间拉满，表示没有收到新的消息
 
-void Usart0_Init(uint32_t bound){
-    //
+u8 UART1_RX_BUF[UART_REC_LEN] = {0};  //串口接收缓冲区,最大UART_REC_LEN个字节.
+u16 UART1_RX_STA = 0;//接收到的有效字节数目
+u8 uart1RXTime = 0xFF;//串口消息间隔计时，初始化把时间拉满，表示没有收到新的消息
+
+//初始化串口0和中断
+void Uart0_Init(uint32_t bound){
     // Enable the GPIO Peripheral used by the UART.
-    //
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
-    //
     // Enable UART0
-    //
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
 
-    //
     // Configure GPIO Pins for UART mode.
-    //
     MAP_GPIOPinConfigure(GPIO_PA0_U0RX);
     MAP_GPIOPinConfigure(GPIO_PA1_U0TX);
     MAP_GPIOPinTypeUART(GPIOA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
-    //
     // Use the internal 16MHz oscillator as the UART clock source.
-    //
     UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
 
-    //
     // Initialize the UART for console I/O.
-    //
     UARTStdioConfig(0, bound, 16000000);
 
+    // Set usart int priorityset
+    IntPrioritySet(INT_UART0,2);
+
+    // Enable UART receive interrupt and receive timeout interrupt
+    UARTIntEnable(UART0_BASE,UART_INT_RX | UART_INT_RT);
+
+    // Enable UART0 interrupt
+    MAP_IntEnable(INT_UART0);
+}
+
+//初始化串口1和中断
+void Uart1_Init(uint32_t bound){
+    // Enable the GPIO Peripheral used by the UART.
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+
+    // Enable UART0
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+
+    // Configure GPIO Pins for UART mode.
+    MAP_GPIOPinConfigure(GPIO_PC4_U1RX);
+    MAP_GPIOPinConfigure(GPIO_PC5_U1TX);
+    MAP_GPIOPinTypeUART(GPIOC_BASE, GPIO_PIN_4 | GPIO_PIN_5);
+
+    // Use the internal 16MHz oscillator as the UART clock source.
+    MAP_UARTClockSourceSet(UART1_BASE, UART_CLOCK_PIOSC);
+
+    // Initialize the UART for console I/O.
+    UARTStdioConfig(1, bound, 16000000);//16Mhz
+
+    // Set usart int priorityset
+    MAP_IntPrioritySet(INT_UART1,2);
+
+    // Enable UART receive interrupt and receive timeout interrupt
+    MAP_UARTIntEnable(UART1_BASE,UART_INT_RX | UART_INT_RT);
+
+    // Enable UART0 interrupt
+    MAP_IntEnable(INT_UART1);
 }
--- MiniCar_V2/Hardwares/usart/usart.h
+++ MiniCar_V2/Hardwares/usart/usart.h
@@ -2,5 +2,17 @@
 #define __USART_H			  	 
 #include "sys.h"
 #include "stdio.h"
-void Usart0_Init(uint32_t bound);
+
+#define UART_REC_LEN 200 // 表示串口消息缓冲区大小
+
+extern u8 UART0_RX_BUF[UART_REC_LEN];//串口接收缓冲区,最大UART_REC_LEN个字节.
+extern u16 UART0_RX_STA;//接收到的有效字节数目
+extern u8 uart0RXTime;
+
+extern u8 UART1_RX_BUF[UART_REC_LEN];//串口接收缓冲区,最大UART_REC_LEN个字节.
+extern u16 UART1_RX_STA;//接收到的有效字节数目
+extern u8 uart1RXTime;
+
+void Uart0_Init(uint32_t bound);
+void Uart1_Init(uint32_t bound);
 #endif  
