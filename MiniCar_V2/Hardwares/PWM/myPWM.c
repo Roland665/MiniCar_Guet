aad16dc605d7250795d1c8934e0f8bde6497bd37
l18005973661 <3051512493@qq.com>
2023-07-11 17:13:11 +0800

    Submit the tested PWM initialize

--- MiniCar_V2/Hardwares/PWM/myPWM.c
+++ MiniCar_V2/Hardwares/PWM/myPWM.c
@@ -2,18 +2,105 @@
 #include "HC_SR04/hc_sr04.h"
 
 /**
-  * @brief    PWM模块0通道2初始化
-  * @param    period    :pwm时钟分频系数
-  * @param    period    :pwm周期
+  * @brief    PWM模块0通道0初始化
+  * @param    PWM_CLKDIV    :pwm时钟分频器(对系统时钟分频)
+  * @param    period    :pwm周期(单位是1 系统时钟周期*分频系数)
   * @param	  width     :pwm脉冲宽度
   * @retval   void
   */
-void M0PWM2_Init(u16 period, u16 width){
-	//配置PWM时钟（设置USEPWMDIV分频器）
-	SysCtlPWMClockSet(SYSCTL_PWMDIV_1);
+void M0PWM0_Init(u32 PWM_CLKDIV, u16 period, u16 width){
+	//配置PWM时钟（设置USEPWM_CLKDIV分频器）
+	SysCtlPWMClockSet(PWM_CLKDIV);
 	
 	//使能时钟
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);			//使能PWM模块1时钟																	
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);	//使能GPIO时钟	
+	
+	//使能引脚复用PWM功能
+	GPIOPinTypePWM(GPIOB_BASE,GPIO_PIN_6);
+
+	//PWM信号分配
+	GPIOPinConfigure(GPIO_PB6_M0PWM0);					//PB4->PWM模块0信号2																						
+
+	//配置PWM发生器
+	//模块0->发生器1->下计数，不同步
+	PWMGenConfigure(PWM0_BASE,PWM_GEN_0,PWM_GEN_MODE_DOWN|PWM_GEN_MODE_NO_SYNC);	
+    
+	//配置PWM周期
+	/*
+	N = (1 / f) * SysClk。
+	其中N是函数参数，即PWM周期(以系统时间为单位),最大为65535, 也可以看做计数器重装载值
+	f为期望频率，
+	SysClk为系统时钟频率。
+	*/
+	PWMGenPeriodSet(PWM0_BASE,PWM_GEN_0, period);			//pwm周期													
+
+	//配置PWM占空比
+	PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, width);		//脉冲长度
+
+	//使能PWM模块0输出
+	PWMOutputState(PWM0_BASE,PWM_OUT_0_BIT,true);
+
+    PWMGenEnable(PWM0_BASE,PWM_GEN_0);
+}
+
+/**
+  * @brief    PWM模块0通道1初始化
+  * @param    PWM_CLKDIV    :pwm时钟分频器(对系统时钟分频)
+  * @param    period    :pwm周期(单位是1 系统时钟周期*分频系数)
+  * @param	  width     :pwm脉冲宽度
+  * @retval   void
+  */
+void M0PWM1_Init(u32 PWM_CLKDIV, u16 period, u16 width){
+	//配置PWM时钟（设置USEPWM_CLKDIV分频器）
+	SysCtlPWMClockSet(PWM_CLKDIV);
+	
+	//使能时钟
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);			//使能PWM模块1时钟																	
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);	//使能GPIO时钟	
+	
+	//使能引脚复用PWM功能
+	GPIOPinTypePWM(GPIOB_BASE,GPIO_PIN_7);
+
+	//PWM信号分配
+	GPIOPinConfigure(GPIO_PB7_M0PWM1);					//PB4->PWM模块0信号2																						
+
+	//配置PWM发生器
+	//模块0->发生器1->下计数，立即更新
+	PWMGenConfigure(PWM0_BASE,PWM_GEN_0,PWM_GEN_MODE_DOWN|PWM_GEN_MODE_NO_SYNC);	
+
+	//配置PWM周期
+	/*
+	N = (1 / f) * SysClk。
+	其中N是函数参数，即PWM周期(以系统时间为单位),最大为65535, 也可以看做计数器重装载值
+	f为期望频率，
+	SysClk为系统时钟频率。
+	*/
+	PWMGenPeriodSet(PWM0_BASE,PWM_GEN_0, period);			//pwm周期													
+
+	//配置PWM占空比
+	PWMPulseWidthSet(PWM0_BASE, PWM_OUT_1, width);		//脉冲长度
+
+	//使能PWM模块0输出
+	PWMOutputState(PWM0_BASE,PWM_OUT_1_BIT,true);
+
+    PWMGenEnable(PWM0_BASE,PWM_GEN_0);
+}
+
+
+/**
+  * @brief    PWM模块0通道2初始化
+  * @param    PWM_CLKDIV    :pwm时钟分频器(对系统时钟分频)
+  * @param    period    :pwm周期(单位是1 系统时钟周期*分频系数)
+  * @param	  width     :pwm脉冲宽度
+  * @retval   void
+  */
+void M0PWM2_Init(u32 PWM_CLKDIV, u16 period, u16 width){
+	//配置PWM时钟（设置分频器）
+	SysCtlPWMClockSet(PWM_CLKDIV);
+	
+	//使能时钟
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);			//使能PWM模块0时钟																	
 	SysCtlPeripheralEnable(HC_SR04_TRIG_GPIOPERIPH);	//使能GPIO时钟	
 	
 	//使能引脚复用PWM功能
